0b05f9fb673abe6887296f9d6f3f5fff4140eaf9
Bryan2397 <bryan.mirtil@gmail.com>
2026-03-25 06:00:26 -0400

    added authprovider to use the global state anywhere and bootstrap js for interactivity

--- internship-tracker/src/main.tsx
+++ internship-tracker/src/main.tsx
@@ -3,11 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { AuthProvider } from "./context/AuthProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>,
 );
